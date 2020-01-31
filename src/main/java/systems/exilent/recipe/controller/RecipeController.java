1ba71ece1c0be4adec54d177d833590b840784b8
Paras <vij@exilent.systems>
2020-01-31 13:07:33 +0530

    Recipe Find by id

--- src/main/java/systems/exilent/recipe/controller/RecipeController.java
+++ src/main/java/systems/exilent/recipe/controller/RecipeController.java
@@ -4,10 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import systems.exilent.recipe.model.Recipe;
 import systems.exilent.recipe.repository.RecipeRepository;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/recipe")
@@ -29,5 +32,15 @@ public class RecipeController {
         recipeRepository.save(recipe);
         model.addAttribute("recipes",recipeRepository.findAll());
         return "recipe-list";
+    }
+
+    @GetMapping("/find/{id}")
+    public String getRecipeById(@PathVariable("id") Long id, Model model) {
+        Optional<Recipe> recipe =  recipeRepository.findById(id);
+        if(!recipe.isPresent()){
+            return "recipe-not-found";
+        }
+        model.addAttribute("recipe",recipe.get());
+        return "show-recipe";
     }
 }
