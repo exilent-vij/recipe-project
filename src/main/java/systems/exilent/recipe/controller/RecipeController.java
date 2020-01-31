ccb245299f15f436b2875bbc8374d6039cfad8f9
Paras <vij@exilent.systems>
2020-01-31 11:22:58 +0530

    Recipe create

--- src/main/java/systems/exilent/recipe/controller/RecipeController.java
+++ src/main/java/systems/exilent/recipe/controller/RecipeController.java
@@ -4,7 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import systems.exilent.recipe.model.Recipe;
 import systems.exilent.recipe.repository.RecipeRepository;
 
 @Controller
@@ -15,6 +17,17 @@ public class RecipeController {
     @GetMapping("/show")
     public String getRecipes(Model model){
         model.addAttribute("recipes", recipeRepository.findAll());
+        return "recipe-list";
+    }
+    @GetMapping("/add")
+    public String addRecipeForm(Recipe recipe) {
+        return "create-recipe";
+    }
+
+    @PostMapping("/add")
+    public String addRecipe(Recipe recipe,Model model) {
+        recipeRepository.save(recipe);
+        model.addAttribute("recipes",recipeRepository.findAll());
         return "recipe-list";
     }
 }
