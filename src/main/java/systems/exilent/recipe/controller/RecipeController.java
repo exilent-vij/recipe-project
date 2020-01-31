a5bd47d072f1b7a8cedf10cf038ec0b751960858
anubhav <anubhav@exilent.systems>
2020-01-31 12:59:00 +0530

    Category

--- src/main/java/systems/exilent/recipe/controller/RecipeController.java
+++ src/main/java/systems/exilent/recipe/controller/RecipeController.java
@@ -14,20 +14,22 @@ import systems.exilent.recipe.repository.RecipeRepository;
 public class RecipeController {
     @Autowired
     private RecipeRepository recipeRepository;
+
     @GetMapping("/show")
-    public String getRecipes(Model model){
+    public String getRecipes(Model model) {
         model.addAttribute("recipes", recipeRepository.findAll());
         return "recipe-list";
     }
+
     @GetMapping("/add")
     public String addRecipeForm(Recipe recipe) {
         return "create-recipe";
     }
 
     @PostMapping("/add")
-    public String addRecipe(Recipe recipe,Model model) {
+    public String addRecipe(Recipe recipe, Model model) {
         recipeRepository.save(recipe);
-        model.addAttribute("recipes",recipeRepository.findAll());
+        model.addAttribute("recipes", recipeRepository.findAll());
         return "recipe-list";
     }
 }
